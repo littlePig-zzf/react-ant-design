089c173f689886d3a24b3d4eb7c5babb730f6636
littlePig <1193639712@qq.com>
2018-08-06 15:16:16 +0800

    完成笔触宽度以及橡皮擦宽度的修改

--- src/view/canvas/index.js
+++ src/view/canvas/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Button, Select } from "antd";
+import { Button, Slider } from "antd";
 import "rc-color-picker/assets/index.css";
 import ColorPicker from "rc-color-picker";
 import "./index.css"
 import draw from "./canvas"
 const ButtonGroup = Button.Group;
-const Option = Select.Option;
+// const Option = Select.Option;
 
 let canvasFrame = ''
 class canvas extends Component {
@@ -21,7 +21,7 @@ class canvas extends Component {
       { id: "new", icon: "sync", txt: '新建' },
     ],
     strokeWidth: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    defaultLineWidth: "2px",
+    defaultLineWidth: 2,
     lineColor: "#000",
     curSelect: "pen"
   };
@@ -35,17 +35,13 @@ class canvas extends Component {
     });
   }
   changeColorHandler(obj) {
-    console.log(canvasFrame);
     this.setState({
       lineColor: obj.color
     });
   }
   render() {
-    const strokeOptions = this.state.strokeWidth.map(item => (
-      <Option key={item}>{item}px</Option>
-    ));
     const buttonItem = this.state.paintTool.map(item => (
-      <Button key={item.id} id={item.id} className={ this.state.curSelect === item.id ? 'active': ''} icon={item.icon} onClick={
+      <Button key={item.id} id={item.id} className={this.state.curSelect === item.id ? 'active': ''} icon={item.icon} onClick={
         () => {
           this.setState({
             curSelect: item.id
@@ -59,25 +55,23 @@ class canvas extends Component {
       <div className="container">
         <h4>开启你的涂鸦之路！</h4>
         <div className="canvas-box">
-          <div className="paint-tool" id="select">
-            <ButtonGroup>
+          <div className="paint-tool">
+            <ButtonGroup id="select">
                 {buttonItem}
             </ButtonGroup>
             <label className="stroke-width">
-              笔触宽度：
+              {this.state.curSelect === 'robber' ? '橡皮擦宽度' : '笔触宽度'}：
               <input
                 id="lineWidth"
                 placeholder={this.state.defaultLineWidth}
                 type="number"
                 style={{ display: "none" }}
               />
-              <Select
-                defaultValue={this.state.defaultLineWidth}
-                onSelect={this.changeLineWidth.bind(this)}
-                style={{ width: 120 }}
-              >
-                {strokeOptions}
-              </Select>
+              <Slider defaultValue={this.state.defaultLineWidth} tipFormatter={
+                (value) => {
+                  return `${value}px`
+                }
+              } style={{ width: 200, display: 'inline-block', verticalAlign: 'middle', marginTop: 10 }} onChange={this.changeLineWidth.bind(this)}/>
             </label>
             <label className="stroke-width">
               笔触颜色：
@@ -104,7 +98,7 @@ class canvas extends Component {
           <div className="canvas-con">
             <canvas
               id="penal"
-              width="1000"
+              width="1100"
               height="500"
               style={{ border: "1px solid #eee" }}
             />
